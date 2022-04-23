432f9d2494d04a237308a89b674d68738701dc76
Makhmudov Alijon <98184267+alijonmakhmudov@users.noreply.github.com>
2022-04-23 11:19:24 +0500

    Delete lib directory

--- lib/theem.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
- class Themes extends GetxController{
-  Color balck=Colors.black;
-  Color red=Colors.red;
-  Color white=Colors.black;
-  var username=''.obs;
-}
